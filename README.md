deef144d395e6cfe4e96d4de681b51b284b5ee7c
ChaddFrasier <33643771+ChaddFrasier@users.noreply.github.com>
2020-04-27 09:57:52 -0700

    comments and todos

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # Neb-usgs
-General examples and information to help new users of the USGS Astrogeology Science Center's Supercomputer: Nebula
+General examples and information to help new users of the USGS Astrogeology Science
+
+## Introduction
+Intro to Nebula and what it is and what it is used for.
+
+## Prerequisites
+List of things that need to be accomplished.
+
+## Resources
+Resources for new users to learn more quickly
+
+### Examples
+Examples of whole scripts or config commands / setup
+
+### Links
+Links that will help a new user learn how to use the cluster more effectively.
--- scripts/getDl.sh
+++ scripts/getDl.sh
@@ -1,11 +1,16 @@
 #!/bin/sh : script for downloading all files given by 'bash getDL.sh <path/to/filename.txt>
 # ** ONLY FOR LINUX **
 
+# THIS COULD BE DONE USING PYTHON for any OS with python env
+# TODO: allow this script to download the files from the list to either $pwd or a new location
+# USAGE: download to path -> python ./downloadHTTP.txt -o <output/folder/location>
+# USAGE: download to pwd -> python ./downloadHTTP.txt
+
 # get cores present in machine
 usablecores=$(echo $(nproc --all))
 
 # echo cores being used
 echo "Bash: Using $usablecores Cores For Download" && echo
 
-# move to $LUNARDATA path and then call wget with multiple processes ignore file that already exist
-cd $LUNARDATA && xargs -n 1 -P $usablecores wget -nc -q < $1
+# call wget with multiple processes ignore file that already exist
+xargs -n 1 -P $usablecores wget -nc -q < $1
